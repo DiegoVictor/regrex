650777511a7832e5ce89ad1a13242a878759630f
Diego Victor <diegovictorgonzaga@gmail.com>
2022-07-08 18:13:19 -0300

    Extract flags and terms

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -1,3 +1,38 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+
+const flagsMap = [
+  'd',
+  'digits',
+  'D',
+  'non-digits',
+  's',
+  'spaces',
+  'S',
+  'non-spaces',
+  'w',
+  'words',
+  'W',
+  'non-words',
+  'r',
+  'repetitions',
+  'e',
+  'escape',
+  'with-surrogates',
+  'i',
+  'ignore-case',
+  'g',
+  'capture-groups',
+  'x',
+  'verbose',
+  'no-start-anchor',
+  'no-end-anchor',
+  'no-anchors',
+];
+
 export const main = async (event: APIGatewayProxyEvent) => {
+  const { terms, flags } = JSON.parse(event.body);
+
+  const parsedFlags = flags
+    .filter((key: string) => flagsMap.includes(key))
+    .map((flag: string) => `${flag.length > 1 ? '--' : '-'}${flag}`);
 };
