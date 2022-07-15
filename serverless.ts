fefa5aa22fd3452de1fcb253f63df385b42d04fb
Diego Victor <diegovictorgonzaga@gmail.com>
2022-07-15 19:19:27 -0300

    Configure lambda

--- serverless.ts
+++ serverless.ts
@@ -1,5 +1,7 @@
 import type { AWS } from '@serverless/typescript';
 
+import main from '@functions/main';
+
 const serverlessConfiguration: AWS = {
   service: 'regrex',
   frameworkVersion: '3',
@@ -16,6 +18,8 @@ const serverlessConfiguration: AWS = {
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
   },
+  functions: { main },
+  package: { individually: true, exclude: ['dependencies'] },
   custom: {
     esbuild: {
       bundle: true,
