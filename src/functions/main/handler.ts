1b2960dc39c0b52c3a2c6e280812a3c9cdc63ee2
Diego Victor <diegovictorgonzaga@gmail.com>
2022-07-19 20:11:15 -0300

    Import validator

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -2,34 +2,8 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { promisify } from 'util';
 import { exec } from 'child_process';
 
-const flagsMap = [
-  'd',
-  'digits',
-  'D',
-  'non-digits',
-  's',
-  'spaces',
-  'S',
-  'non-spaces',
-  'w',
-  'words',
-  'W',
-  'non-words',
-  'r',
-  'repetitions',
-  'e',
-  'escape',
-  'with-surrogates',
-  'i',
-  'ignore-case',
-  'g',
-  'capture-groups',
-  'x',
-  'verbose',
-  'no-start-anchor',
-  'no-end-anchor',
-  'no-anchors',
-];
+import * as validate from '@application/validators/grexParams';
+import { ZodError } from 'zod';
 
 export const main = async (event: APIGatewayProxyEvent) => {
   const { terms, flags } = JSON.parse(event.body);
