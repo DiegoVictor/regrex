0720ef382b1d542ff2e09cfdcd92c311abfaf5ff
Diego Victor <diegovictorgonzaga@gmail.com>
2022-07-19 20:11:40 -0300

    Validate payload

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -6,11 +6,10 @@ import * as validate from '@application/validators/grexParams';
 import { ZodError } from 'zod';
 
 export const main = async (event: APIGatewayProxyEvent) => {
-  const { terms, flags } = JSON.parse(event.body);
+  try {
+    const body = JSON.parse(event.body || '{}');
 
-  const parsedFlags = flags
-    .filter((key: string) => flagsMap.includes(key))
-    .map((flag: string) => `${flag.length > 1 ? '--' : '-'}${flag}`);
+    const { flags = [], terms } = validate.grexParams(body);
 
   const cmd = `grex ${parsedFlags.join(' ')} "${terms.join('" "')}"`;
 
@@ -24,4 +23,6 @@ export const main = async (event: APIGatewayProxyEvent) => {
     },
     body: stdout,
   };
+  } catch (err) {
+  }
 };
