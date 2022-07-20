3eaa848cf1bae34020d2462cf9803527dae0f615
Diego Victor <diegovictorgonzaga@gmail.com>
2022-07-20 18:22:41 -0300

    Update tests

--- tests/unit/main.spec.ts
+++ tests/unit/main.spec.ts
@@ -1,10 +1,18 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { ZodError, ZodIssueCode, ZodParsedType } from 'zod';
 import { main } from '../../src/functions/main/handler';
 
 const mockExec = jest.fn();
 jest.mock('util', () => ({
   promisify: () => mockExec,
 }));
+
+const mockGrexParams = jest.fn();
+jest.mock('@application/validators/grexParams', () => {
+  return {
+    grexParams: (params: Record<string, any>) => mockGrexParams(params),
+  };
+});
 
 describe('Main Function', () => {
   it('should be able to exec command and get a regex', async () => {
@@ -26,6 +34,8 @@ describe('Main Function', () => {
       stdout,
     });
 
+    mockGrexParams.mockReturnValueOnce({ terms, flags });
+
     const response = await main({
       body: JSON.stringify({
         terms,
@@ -44,6 +54,93 @@ describe('Main Function', () => {
         'Content-Type': 'text/plain',
       },
       body: stdout,
+    });
+  });
+
+  it('should not be able to exec command and get a regex with validation errors', async () => {
+    const terms = ['sample', 'example', 'simple'];
+    const flags = ['x', 'ignore-case'];
+
+    const stdout = `
+    (?x)
+    ^
+      (?:
+        exa
+        |
+        s[ai]
+      )
+      mple
+    $
+    `;
+    mockExec.mockResolvedValueOnce({
+      stdout,
+    });
+
+    const error = {
+      message: 'fake error message',
+      code: ZodIssueCode.invalid_type,
+      expected: ZodParsedType.integer,
+      received: ZodParsedType.boolean,
+      path: [''],
+    };
+    const zodError = new ZodError([error]);
+    mockGrexParams.mockImplementationOnce(() => {
+      throw zodError;
+    });
+
+    const response = await main({
+      body: JSON.stringify({
+        terms,
+        flags,
+      }),
+    } as APIGatewayProxyEvent);
+
+    expect(mockExec).not.toHaveBeenCalled();
+    expect(response).toStrictEqual({
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Validation fail',
+        details: zodError.issues,
+      }),
+    });
+  });
+
+  it('should not be able to exec command and get a regex with validation errors', async () => {
+    const terms = ['sample', 'example', 'simple'];
+    const flags = ['x', 'ignore-case'];
+
+    const stdout = `
+    (?x)
+    ^
+      (?:
+        exa
+        |
+        s[ai]
+      )
+      mple
+    $
+    `;
+    mockExec.mockResolvedValueOnce({
+      stdout,
+    });
+
+    mockGrexParams.mockImplementationOnce(() => {
+      throw new Error('Unexpected Error');
+    });
+
+    const response = await main({
+      body: JSON.stringify({
+        terms,
+        flags,
+      }),
+    } as APIGatewayProxyEvent);
+
+    expect(mockExec).not.toHaveBeenCalled();
+    expect(response).toStrictEqual({
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Ops! Something goes wrong, try again later.',
+      }),
     });
   });
 });
