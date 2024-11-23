855da48f58c007eaa8f65e85ff76ea8eab4285dd
Diego Victor <diegovictorgonzaga@gmail.com>
2024-11-23 16:15:54 -0300

    Upgrade node version

--- serverless.ts
+++ serverless.ts
@@ -8,7 +8,7 @@ const serverlessConfiguration: AWS = {
   plugins: ['serverless-esbuild'],
   provider: {
     name: 'aws',
-    runtime: 'nodejs14.x',
+    runtime: 'nodejs18.x',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
